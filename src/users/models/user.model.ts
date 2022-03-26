b75c054cba84e2a3e17d142882dc9bc957c80b08
Brice Duke <bricewduke@gmail.com>
2022-03-26 11:15:10 -0500

    feat(users): add user models

---
+++ src/users/models/user.model.ts
@@ -0,0 +1,9 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+
+import { AbstractModel } from '../../common/abstract.model';
+
+@ObjectType()
+export class User extends AbstractModel {
+	@Field()
+	readonly email: string;
+}
---
+++ src/users/resolvers/users.resolver.ts
@@ -0,0 +1,8 @@
+import { Resolver } from '@nestjs/graphql';
+
+import { UsersService } from '../services/users.service';
+
+@Resolver()
+export class UsersResolver {
+	constructor(private readonly usersService: UsersService) {}
+}
