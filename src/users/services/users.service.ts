ae49d8a095473c7c92e2b3119baadd6359739c1d
Brice Duke <bricewduke@gmail.com>
2022-03-26 11:33:27 -0500

    feat(users): add users service

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException, UnprocessableEntityException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+import { GetUserArgs } from '../dto/args/get-user.dto';
+import { CreateUserInput } from '../dto/input/create-user.input';
+import { User } from '../models/user.model';
+import { UserDocument } from '../models/user.schema';
+import { UsersRepository } from '../repositories/users.repository';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+	constructor(private readonly usersRepo: UsersRepository) {}
+
+	private async validateData(data: CreateUserInput) {
+		try {
+			await this.usersRepo.findOne({ email: data.email });
+			throw new UnprocessableEntityException(
+				"you already have an account silly"
+			);
+		} catch (err) {}
+	}
+
+	private toModel(userDoc: UserDocument): User {
+		return {
+			_id: userDoc._id.toHexString(),
+			email: userDoc.email,
+		};
+	}
+
+	async createUser(data: CreateUserInput) {
+		await this.validateData(data);
+
+		const userDoc = await this.usersRepo.create({
+			...data,
+			password: await bcrypt.hash(data.password, 12),
+		});
+
+		return this.toModel(userDoc);
+	}
+
+	async getUser(args: GetUserArgs) {
+		const userDoc = await this.usersRepo.findOne(args);
+
+		return this.toModel(userDoc);
+	}
+
+	async validateUser(email: string, password: string) {
+		const userDoc = await this.usersRepo.findOne({ email });
+
+		const validPassword = await bcrypt.compare(password, userDoc.password);
+
+		if (!validPassword)
+			throw new UnauthorizedException(
+				"nice try but you got the wrong password idiot"
+			);
+
+		return this.toModel(userDoc);
+	}
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
+import { User } from './models/user.model';
+import { UserSchema } from './models/user.schema';
 import { UsersResolver } from './resolvers/users.resolver';
 import { UsersService } from './services/users.service';
 
 @Module({
+	imports: [
+		MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+	],
 	providers: [UsersResolver, UsersService],
+	exports: [UsersService],
 })
 export class UsersModule {}
