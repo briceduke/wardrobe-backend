6b4f6d731b71e9d0ad688af5685ddbebeee38edb
Brice Duke <bricewduke@gmail.com>
2022-03-26 12:01:31 -0500

    feat(auth): add auth service

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
+import { User } from 'src/users/models/user.model';
+
+import { TokenPayload } from '../dto/token-payload.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	constructor(
+		private readonly configService: ConfigService,
+		private readonly jwtService: JwtService
+	) {}
+
+	async login(user: User, res: Response) {
+		const payload: TokenPayload = {
+			userId: user._id,
+		};
+
+		const exp = new Date();
+
+		exp.setSeconds(exp.getSeconds() + this.configService.get("JWT_EXP"));
+
+		const token = this.jwtService.sign(payload);
+
+		res.cookie("Authentication", token, {
+			httpOnly: true,
+			expires: exp,
+		});
+	}
+
+	logout(res: Response) {
+		res.cookie("Authentication", "", {
+			httpOnly: true,
+			expires: new Date(),
+		});
+	}
+}
