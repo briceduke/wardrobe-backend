d640727da22ed974e84b41168ab6699951d9c4e3
Brice Duke <bricewduke@gmail.com>
2022-03-26 11:54:00 -0500

    feat(auth): add local strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from 'src/users/users.module';
 
 import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
+	imports: [
+		UsersModule,
+		JwtModule.registerAsync({
+			useFactory: (configService: ConfigService) => ({
+				secret: configService.get<string>("JWT_SECRET"),
+				signOptions: {
+					expiresIn: `${configService.get("JWT_EXP")}s`,
+				},
+			}),
+			inject: [ConfigService],
+		}),
+	],
 	controllers: [AuthController],
-	providers: [AuthService],
+	providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
