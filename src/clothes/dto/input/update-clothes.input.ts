6ec54f11a328f284e34afe040470c3a6f3ef069d
Brice Duke <bricewduke@gmail.com>
2022-04-03 21:27:47 -0500

    feat(clothes): add dto

---
+++ src/clothes/dto/input/update-clothes.input.ts
@@ -0,0 +1,15 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { FileUpload, GraphQLUpload } from 'graphql-upload';
+
+@InputType()
+export class UpdateClothesInput {
+	@Field()
+	@IsNotEmpty()
+	@IsString()
+	name: string;
+
+	@Field(() => GraphQLUpload)
+	@IsNotEmpty()
+	file: FileUpload;
+}
